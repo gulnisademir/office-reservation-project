acc76d9d17df3e906e79af3f1c96806bd86a571f
gulnisademir <123929569+gulnisademir@users.noreply.github.com>
2023-03-02 00:06:00 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
 import React, { useState } from "react";
 
-import './App.css';
-import { Login } from "./Login";
-import { Register } from "./Register";
+import "./App.css";
+import { Login } from "./componenets/Login";
+import { Register } from "./componenets/Register";
 
 import Users from "./Users";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
+  const [currentForm, setCurrentForm] = useState("login");
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
-  }
-
-  
+  };
 
   return (
     <div className="App">
-
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
-      
-      <hr/>
-      <Users/>
-        
-      
-
-
-
+      {currentForm === "login" ? (
+        <Login onFormSwitch={toggleForm} />
+      ) : (
+        <Register onFormSwitch={toggleForm} />
+      )}
     </div>
-
-
-
-);
+  );
 }
 
 export default App;
